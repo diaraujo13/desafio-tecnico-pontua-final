db2c2db198d22ef26856bfb0c0d78c4b3f905ed4
Izaías Araújo <izaiasousa@gmail.com>
2025-12-24 09:39:14 -0300

    feat: Export vacation hooks

---
+++ src/presentation/hooks/vacations/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Barrel file for vacation hooks
+ * Exports all vacation-related hooks for easy importing
+ */
+
+export { useRequestVacation } from './useRequestVacation';
+export { useApproveVacation } from './useApproveVacation';
+export { useRejectVacation } from './useRejectVacation';
+export { useCancelVacation } from './useCancelVacation';
+export { useVacationHistory } from './useVacationHistory';
+export { useManagerDashboard } from './useManagerDashboard';
+export { useAllVacations } from './useAllVacations';
