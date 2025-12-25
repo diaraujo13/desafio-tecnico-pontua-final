b365d1681d9a9d35e96775f695b4368af0212154
Izaías Araújo <izaiasousa@gmail.com>
2025-12-25 19:32:17 -0300

    refactor: streamline release_core lane and update README

--- fastlane/helpers/versioning.rb
+++ fastlane/helpers/versioning.rb
@@ -1,27 +1,9 @@
 module Versioning
   def self.version
     JSON.parse(File.read("../package.json"))["version"]
-    UI.message("Version: #{version}")
-  end
-
-  def self.bump!(xcodeproj:)
-    UI.message("Bumping version for #{xcodeproj}")
-    increment_build_number(xcodeproj: xcodeproj)
-    increment_version_number(
-      xcodeproj: xcodeproj,
-      version_number: version
-    )
-
-    UI.message("Setting versions for Android")
-    Android.set_versions(
-      version_name: version,
-      version_code: get_build_number(xcodeproj: xcodeproj)
-    )
   end
 
   def self.update_changelog!(tag:, changelog:)
-    UI.message("Updating changelog for #{tag}")
-    
     entry = <<~MD
 
       ## #{tag}
@@ -30,8 +12,7 @@ module Versioning
     MD
 
     if File.exist?("CHANGELOG.md")
-      content = File.read("CHANGELOG.md")
-      File.write("CHANGELOG.md", entry + content)
+      File.write("CHANGELOG.md", entry + File.read("CHANGELOG.md"))
     else
       File.write("CHANGELOG.md", "# Changelog\n#{entry}")
     end
